33860a742e924776c5d672015aa4df05e4915f64
Pratham Agarwal <109911989+UniquePratham@users.noreply.github.com>
2023-10-19 10:03:17 +0530

    5th Changes for Vercel

--- pages/api/SignUpReq&Res.js
+++ pages/api/SignUpReq&Res.js
@@ -11,22 +11,23 @@ export default async function handler(req, res) {
     const check = await User.findOne({ email: data.email });
     if (check) {
       return res
-        .status(406)
-        .json({ success: false, error: "Email already registered" });
+      .status(406)
+      .json({ success: false, error: "Email already registered" });
     } else {
       const hashedPassword = await bcrypt.hash(data.password, 8);
       data.password=`${hashedPassword}`;
       const user = await User.create(data);
       const savedUser = await user.save();
+      res.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
       res.status(200).json({
-        "success": true,
-        "data": savedUser,
-        "message": "User created successfully"
+        success: true,
+        data: savedUser,
+        message: "User created successfully"
       });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ "success": false, "error": "Server error" });
+    res.status(500).json({ success: false, error: "Server error" });
   }
   // } else {
   //   res.status(405).json({ success: false, error: "Method not allowed" });
