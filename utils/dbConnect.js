49060decbf6521fdf156f495b4d14d1398bac060
Pratham Agarwal <gargpratham71@gmail.com>
2023-11-26 18:54:14 +0530

    ok

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,21 +1,41 @@
 import mongoose from "mongoose";
-const username = encodeURIComponent("UniquePratham");
-const password = encodeURIComponent("LujtTkl879XO1q27");
-const cluster = "cloudycluster";
-const dbConnect = async () =>
-  // mongoose.connect("mongodb://localhost:27017/signUpDatabase");
-  mongoose.connect(
-    `mongodb+srv://${username}:${password}@${cluster}.ygyavyt.mongodb.net/`
+const MONGODB_URI =
+  "mongodb+srv://UniquePratham:LujtTkl879XO1q27@cloudycluster.ygyavyt.mongodb.net/signupdatabase?retryWrites=true&w=majority";
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside the .env.local"
   );
+}
+let cached = global.mongoose;
+if (!cached) {
+  cached = global.mongoose = { con: null, promise: null };
+}
+const dbConnect = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
 
-// if (!mongoose.connections[0]) {
-//   mongoose.connect(MONGODB_URI, {
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true,
-//     useNewUrlParser: true,
-//   });
-// }
-console.log("We are connected to MongoDB");
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+  console.log("We are connected to MongoDB");
+  return cached.conn;
+  // mongoose.connect("mongodb://localhost:27017/signUpDatabase");
+  // mongoose.connect(
+  //   // `mongodb+srv://${username}:${password}@${cluster}.ygyavyt.mongodb.net/`
+  // );
+};
 
 export default dbConnect;
